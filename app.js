a483d29686c1741745359f7003d1fba2324961c4
SarveshtheAbstractor <hiwasesarvesh@yahoo.com>
2020-11-16 14:53:36 +0530

    Adding CORS to API

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://narutoforlifeapi.herokuapp.com/"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const templatesDirectory = path.join(__dirname, "/templates");
 cl(templatesDirectory);
 
