3abcedaa7d578252a1d2d9e3cb529562ebda4663
SarveshtheAbstractor <hiwasesarvesh@yahoo.com>
2020-11-17 13:56:40 +0530

    Adding Styling to API

--- templates/index.js
+++ templates/index.js
@@ -1,33 +1,87 @@
-const SearchForm = document.getElementById("SearchForm");
-const DataAdd = document.getElementById("Data");
+const APIURL = "/api/characters/";
 
-const url = (character) => `/api/characters/${character}`;
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-SearchForm.addEventListener("submit", (e) => {
+getUser("itachi");
+
+async function getUser(username) {
+  const resp = await fetch(APIURL + username);
+  const respData = await resp.json();
+
+  createUserCard(respData);
+
+  addReposToCard(respData);
+}
+
+// async function getRepos(username) {
+//   const resp = await fetch(APIURL + username + "/repos");
+//   const respData = await resp.json();
+
+//   addReposToCard(respData);
+// }
+
+function createUserCard(user) {
+  const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.image_url}" alt="${
+    user.name
+  }" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.occupation.join(" , ")}</p>
+
+                <ul class="info">
+                    <li><strong>Age- </strong> ${user.age}</li>
+                    <li><strong>BirthDate- </strong>${user.birthdate}</li>
+                    <li><strong>Clan- </strong>${user.clan}</li>
+                </ul>
+
+                <div id="classif"><span>Classification - </span></div>
+                <div id="jutsus"><span>Best Jutsus - </span></div>
+            </div>
+        </div>
+    `;
+
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(charac) {
+  const classifEl = document.getElementById("classif");
+  const jutsusEl = document.getElementById("jutsus");
+
+  charac.classifaction.forEach((char) => {
+    const characEl = document.createElement("a");
+    characEl.classList.add("characinfo");
+
+    characEl.alt = char;
+    characEl.innerText = char;
+
+    classifEl.appendChild(characEl);
+  });
+
+  charac.best_jutsus.forEach((char) => {
+    const jutsuEl = document.createElement("a");
+    jutsuEl.classList.add("characinfo");
+
+    jutsuEl.alt = char;
+    jutsuEl.innerText = char;
+
+    jutsusEl.appendChild(jutsuEl);
+  });
+}
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const characterSearch = document.getElementById("charac").value;
-  getcharacterDetails(characterSearch);
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = "";
+  }
 });
-
-const getcharacterDetails = async (charac) => {
-  const results = await fetch(url(charac));
-  const JSONdata = await results.json();
-  console.log(JSONdata);
-  DataAdd.innerHTML = HTMLmarkup(JSONdata);
-};
-
-const HTMLmarkup = (data) => {
-  console.log(data);
-  const markup = `<h1>${data.name} </h1>
-  <img src="${data.image_url}">
-  
-  <p>${data.age}</p>
-  <p>${data.birthdate}</p>
-  <p>${data.clan}</p>
-  <p>${data.best_jutsus.join(",")}<p>
-  <p>${data.classifaction.join(",")}<p>
-  <p>${data.occupation.join(",")}<p>
-  
-  `;
-  return markup;
-};
